76962c1e2e8f3d795b6d72433212a498b1fdfd2c
Dirk Gadsden <dirk@esherido.com>
2018-10-20 15:00:43 -0700

    Experiment with rebuilding Rails classes in C

--- ext/accelerated_record/accelerated_record.c
+++ ext/accelerated_record/accelerated_record.c
@@ -1,9 +1,56 @@
 #include "accelerated_record.h"
 
 VALUE rb_mAcceleratedRecord;
+VALUE rb_cBelongsToAssociation;
 
-void
-Init_accelerated_record(void)
+typedef struct {
+  VALUE owner;
+  VALUE reflection;
+} belongs_to_association_t;
+
+void belongs_to_association_free(void* data)
+{
+	free(data);
+}
+
+size_t belongs_to_association_size(const void* data)
+{
+	return sizeof(belongs_to_association_t);
+}
+
+static const rb_data_type_t belongs_to_association_type = {
+  .wrap_struct_name = "belongs_to_association_t",
+  .function = {
+    .dmark = NULL,
+    .dfree = belongs_to_association_free,
+    .dsize = belongs_to_association_size,
+  },
+  .data = NULL,
+  .flags = RUBY_TYPED_FREE_IMMEDIATELY,
+};
+
+VALUE belongs_to_association_alloc(VALUE self)
+{
+	belongs_to_association_t* data = malloc(sizeof(belongs_to_association_t));
+	return TypedData_Wrap_Struct(self, &belongs_to_association_type, data);
+}
+
+VALUE belongs_to_association_initialize(VALUE self, VALUE owner, VALUE reflection)
+{
+	belongs_to_association_t* data;
+	TypedData_Get_Struct(self, belongs_to_association_t, &belongs_to_association_type, data);
+
+	data->owner = owner;
+  data->reflection = reflection;
+
+	return self;
+}
+
+void Init_accelerated_record(void)
 {
   rb_mAcceleratedRecord = rb_define_module("AcceleratedRecord");
+
+  rb_cBelongsToAssociation = rb_define_class_under(rb_mAcceleratedRecord, "BelongsToAssociation", rb_cObject);
+  rb_define_alloc_func(rb_cBelongsToAssociation, belongs_to_association_alloc);
+	rb_define_method(rb_cBelongsToAssociation, "initialize", belongs_to_association_initialize, 2);
 }
